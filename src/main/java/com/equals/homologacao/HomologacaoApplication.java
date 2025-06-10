5c18bce50a9e6f48b89c760fe9c1ccc14e341da0
Petter <petterdouglas7@gmail.com>
2025-06-09 22:31:51 -0300

    initial commit

---
+++ src/main/java/com/equals/homologacao/HomologacaoApplication.java
@@ -0,0 +1,13 @@
+package com.equals.homologacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomologacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomologacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equals/homologacao/HomologacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equals.homologacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomologacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
