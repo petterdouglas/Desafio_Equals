3098ca2efc5595467814096f2800430f137ecfd0
Petter <petterdouglas7@gmail.com>
2025-06-12 02:00:36 -0300

    refactor: criação de métodos auxiliares para seleção no banco de dados

--- src/main/java/com/equals/homologacao/repository/ExtratoRepository.java
+++ src/main/java/com/equals/homologacao/repository/ExtratoRepository.java
@@ -3,5 +3,11 @@ package com.equals.homologacao.repository;
 import com.equals.homologacao.model.Extrato;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ExtratoRepository extends JpaRepository<Extrato, Long> {
+    List<Extrato> findAllByEmpresaId(Long empresaId);
+
+    Optional<Extrato> findByEmpresaIdAndId(Long empresaId, Long extratoId);
 }
--- src/main/java/com/equals/homologacao/repository/TransacaoRepository.java
+++ src/main/java/com/equals/homologacao/repository/TransacaoRepository.java
@@ -3,5 +3,15 @@ package com.equals.homologacao.repository;
 import com.equals.homologacao.model.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
+    boolean existsByCodigoTransacao(String codigoTransacao);
+
+    List<Transacao> findAllByExtratoId(Long extratoId);
+
+    List<Transacao> findAllByExtratoIdAndCodigoTransacao(Long extratoId, String codigoTransacao);
+
+    Optional<Transacao> findByExtratoIdAndId(Long extratoId, Long transacaoId);
 }
